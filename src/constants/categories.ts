92b476338dfbc69275b6ec07ecc881430ab8fd16
Devan <devan.marcal@gmail.com>
2026-09-08 15:11:29 -0300

    feat: add product categories

---
+++ src/constants/categories.ts
@@ -0,0 +1,13 @@
+export const MEN_CATEGORIES = [
+  'mens-shirts',
+  'mens-shoes',
+  'mens-watches',
+];
+
+export const WOMEN_CATEGORIES = [
+  'womens-bags',
+  'womens-dresses',
+  'womens-jewellery',
+  'womens-shoes',
+  'womens-watches',
+];
